4bc0e966a01ae68302c842ed56a18f23fbfa5b86
camillageber <camilla.geber@gmail.com>
2023-02-02 16:02:34 -0300

    feat: implemented class CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,22 @@
-class CompleteReport:
-    pass
+from collections import Counter
+from typing import List, Dict
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(reports: List[Dict]):
+        simple_report = SimpleReport.generate(reports)
+
+        companies_counter = Counter(
+            [key["nome_da_empresa"] for key in reports]
+        )
+        companies_and_qtd = ""
+        for company_name, quantity in companies_counter.items():
+            companies_and_qtd += f"- {company_name}: {quantity}\n"
+
+        return (
+            f"{simple_report}\n"
+            "Produtos estocados por empresa:\n"
+            f"{companies_and_qtd}"
+        )
